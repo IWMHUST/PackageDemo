1412fd327b8d8fdbd0900508e5e85052a9f9f7ca
Kan Lei <mr_moment@outlook.com>
2020-11-10 00:57:25 +0100

    say hello

---
+++ R/sayHello.R
@@ -0,0 +1,12 @@
+#' @title A function for say hello.
+#' @param txt a txt or char.
+#' @export
+sayHello <- function(txt = "Hello"){
+  print(txt)
+}
+## 0. https://r-pkgs.org/
+## 1. library(devtools)
+## 2. delete environment
+## 3. document()
+## 4. Build--Check
+## 5. BUild--Insatll an Restart
